f54a6579dfbe4b87b5ea40edf298fa6739af4789
Harrison Wynn <harrisonwynn42@gmail.com>
2018-05-20 15:02:55 -0500

    extended pretty printing
    Attempted to use list comprehensions in pretty printing

--- listprinter.py
+++ listprinter.py
@@ -12,3 +12,26 @@ print(printsize)
 print('text speed'.center(printsize))
 for i in range(len(textSpeeds)):
 	print(textSpeeds[i].center(4+len(textSpeeds[i])),end='')
+print()
+
+mylist= [textSpeeds[i].center(4+len(textSpeeds[i])) for i in range(len(textSpeeds))]
+print(mylist)
+
+choice=1
+
+mylist2 = [ ('('+textSpeeds[i]+')').center(4+len(textSpeeds[i])) if i==choice else textSpeeds[i].center(4+len(textSpeeds[i])) for i in range(len(textSpeeds))]
+print('|',end='')
+for x in mylist2:
+	print(x,end='')
+print('|',end='')
+
+print()
+print('|',end='')
+for i in range(len(textSpeeds)):
+	if(i==choice):
+		print(('('+textSpeeds[i]+')').center(4+len(textSpeeds[i])),end='')
+	else:
+		print(textSpeeds[i].center(4+len(textSpeeds[i])),end='')
+
+print('|',end='')
+print()
